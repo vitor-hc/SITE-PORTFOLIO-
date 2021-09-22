9253491501ebde1e7f2546641f133bcd07186221
vitor-hc <vitor_hc@hotmail.com.br>
2021-09-21 21:34:34 -0300

    Versao final

---
+++ static/script.js
@@ -0,0 +1,62 @@
+/*abre e fecha menu*/
+
+const menuMobile = document.querySelector('.menu-mobile')
+const body = document.querySelector('body')
+menuMobile.addEventListener('click', () => {
+    menuMobile.classList.contains("bi-list")
+        ? menuMobile.classList.replace("bi-list", "bi-x") :
+        menuMobile.classList.replace("bi-x", "bi-list");
+    body.classList.toggle("menu-nav-active");
+
+});
+
+
+/* Fecha o menu quando clica no item do menu*/
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
+})
+
+//animar todos os itens na tela coom atribut data-anime="down/up/left/right"
+
+const item = document.querySelectorAll("[data-anime]");
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight *0.85;
+
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add('animate');
+        }
+        else{
+            element.classList.remove('animate');
+        }
+    });
+
+};
+animeScroll();
+
+window.addEventListener("scroll", () => {
+    animeScroll();
+})
+/*
+Ativar o btn enviar */
+const btnEnviar=document.querySelector('#btnEnviar')
+const btnEnviando=document.querySelector('#btnEnviando')
+btnEnviar.addEventListener("click",() =>{
+    btnEnviando.style.display="block";
+    btnEnviar.style.display="none"
+
+})
+
+//Tira o alert
+setTimeout(() =>{
+    document.querySelector('#alerta').style.display='none';
+
+},7000)
